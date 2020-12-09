4645c5b3305b48255a91169b87915b650fa8b424
David Ames <dames@team.nxlink.com>
2020-12-08 21:56:58 -0600

    After work

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request
+from surveys import Question, Survey, satisfaction_survey
 
 app = Flask(__name__)
-app.config['TESTING'] = True
+# app.config['TESTING'] = True
+responses = []
+survey = satisfaction_survey
+
+@app.route('/')
+def fetch_root():
+    return render_template('root.html', instructions=survey.instructions )
+
+@app.route('/questions/<num>')
+def get_question(num):
+    # num = request.args['num']
+    question = satisfaction_survey.questions[int(num)]
+    return render_template('questions.html')
+
+
